990335b1fa1afc4f97ea5d9c19b9560ba3ce0046
Somu050600 <somasekhareega@gmail.com>
2023-08-08 16:35:27 +0530

    fixed intro animation and cards loading in homescreen

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import {
   useWindowDimensions,
   AppState,
   Dimensions,
+  Button,
 } from "react-native";
 import { name as appName } from "./app.json";
 import {
@@ -31,7 +32,7 @@ import ScanScreen from "./App/Screens/ScanScreen";
 import SavedScreen from "./App/Screens/SavedScreen";
 import SettingsScreen from "./App/Screens/SettingsScreen";
 import { useEffect, useRef, useState, useLayoutEffect } from "react";
-import { Video, ResizeMode, Audio, VideoFullscreenUpdateEvent } from "expo-av";
+import { Video, ResizeMode } from "expo-av";
 
 const Stack = createStackNavigator();
 
@@ -39,87 +40,29 @@ export default function App() {
   const [isLoading, setIsLoading] = useState(true);
   const colorScheme = useColorScheme();
   const { theme } = useMaterial3Theme();
-  const video = useRef(null);
-  const [status, setStatus] = useState({});
-  const [appState, setAppState] = useState(AppState.currentState);
-
-  const dimensions = useWindowDimensions();
 
   const paperTheme =
     colorScheme === "dark"
       ? { ...MD3DarkTheme, colors: theme.dark }
       : { ...MD3LightTheme, colors: theme.light };
 
-  const handlePlaybackStatusUpdate = (status) => {
-    setStatus(status);
-    if (status.didJustFinish) {
-      video.current.pauseAsync();
-    }
-  };
-  const handleAppStateChange = async (nextAppState) => {
-    if (appState.match(/inactive|background/) && nextAppState === "active") {
-      // App is coming to the foreground, request audio focus
-      await Audio.setAudioModeAsync({
-        interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DUCK_OTHERS,
-        interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
-      });
-    } else if (nextAppState === "background") {
-      // App is going to the background, release audio focus
-      await Audio.setAudioModeAsync({
-        interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
-        interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
-      });
-    }
-    setAppState(nextAppState);
-  };
-
-  useEffect(() => {
-    AppState.addEventListener("change", handleAppStateChange);
-    return () => {
-      AppState.removeEventListener("change", handleAppStateChange);
-    };
-  }, []);
-
   useLayoutEffect(() => {
-    (async () => {
-      await video.current.loadAsync(
-        require("./App/assets/Video/logo_flow.mp4"),
-        {},
-        false
-      );
-      setStatus(await video.current.getStatusAsync());
-      await video.current.playAsync();
-    })();
-    setTimeout(() => {
+    setTimeout(async () => {
       setIsLoading(false);
     }, 1700);
   }, []);
 
   if (isLoading) {
     return (
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "#fff",
-        }}
-      >
+      <View style={{ flex: 1 }}>
         <Video
-          ref={video}
-          style={{
-            width: dimensions.width + 40,
-            height: dimensions.height + 120,
-          }}
+          style={{ flex: 1 }}
+          source={require("./App/assets/Video/logo_flow.mp4")}
           useNativeControls
-          resizeMode={ResizeMode.CONTAIN}
-          isLooping={false}
-          onPlaybackStatusUpdate={handlePlaybackStatusUpdate}
+          shouldPlay
+          resizeMode={ResizeMode.COVER}
+          isLooping="false"
         />
-        {/* <Image
-          style={{ width: dimensions.width, height: dimensions.height + 40 }}
-          source={require("./App/assets/Video/logo.gif")}
-        /> */}
       </View>
     );
   }
--- App/Screens/HomeScreen.js
+++ App/Screens/HomeScreen.js
@@ -41,7 +41,7 @@ export default function HomeScreen(props) {
 
   useLayoutEffect(() => {
     findCards();
-  }, []);
+  }, [cards]);
 
   useEffect(() => {
     if (refreshFlag) {
